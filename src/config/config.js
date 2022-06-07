5620cddbfd193420dc6edbe60bc31ac2ce0eb696
Shashwath <shashwathbc@gmail.com>
2022-06-07 18:53:19 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,10 @@
 export const img_300 = "https://image.tmdb.org/t/p/w300";
 export const img_500 = "https://image.tmdb.org/t/p/w500";
 
-// contentModal and singleContent
+// TrendingContent and ModalComp
 export const unavailable =
   "https://www.movienewz.com/img/films/poster-holder.jpg";
 
-// contentModal
+// ModalComp
 export const unavailableLandscape =
   "https://user-images.githubusercontent.com/10515204/56117400-9a911800-5f85-11e9-878b-3f998609a6c8.jpg";
-
-// For Carousel
-export const noPicture =
-  "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
